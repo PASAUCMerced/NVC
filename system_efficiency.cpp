f3e7e0fb767ec01836016a2eb34a0861c1057dcb
Jie <37380896+jren73@users.noreply.github.com>
2019-10-03 16:43:29 -0700

    Update system_efficiency.cpp

--- system_efficiency.cpp
+++ system_efficiency.cpp
@@ -11,58 +11,58 @@ int main()
 	
 	for(int i=0;i<3;i++)
 	{
-	double total = 10*365*24*60*60;
-	//int MTBF = 12;
-	//int chk=32*10;
-	if(i!=0)
-		MTBF /=2;
-	cout<<"MTBF = "<<MTBF<<endl;
-	double T = sqrt(2*MTBF*60*60*chk);
-	int M = 10*365*24/MTBF;
-	cout<<"T = "<<T<<endl;
-	double T_w = (3*(chk + T))/2*M;
-	//cout<<"T_w = "<<T_w<<endl;
-	int N1 = (total - T_w)/T +1;
-	int N2 = (total - T_w)/(T + 0.5*chk) +1;
-	int N3 = (total - T_w)/(T + chk) +1;
-	
-	double e1 = T*N1/total;
-	double e2 = T*N2/total;
-	double e3 = T*N3/total;
+		double total = 10*365*24*60*60;
+		//int MTBF = 12;
+		//int chk=32*10;
+		if(i!=0)
+			MTBF /=2;
+		cout<<"MTBF = "<<MTBF<<endl;
+		double T = sqrt(2*MTBF*60*60*chk);
+		int M = 10*365*24/MTBF;
+		cout<<"T = "<<T<<endl;
+		double T_w = (3*(chk + T))/2*M;
+		//cout<<"T_w = "<<T_w<<endl;
+		int N1 = (total - T_w)/T +1;
+		int N2 = (total - T_w)/(T + 0.5*chk) +1;
+		int N3 = (total - T_w)/(T + chk) +1;
 
-	cout<<e1<<endl;
-	cout<<e2<<endl;
-	cout<<e3<<endl;
+		double e1 = T*N1/total;
+		double e2 = T*N2/total;
+		double e3 = T*N3/total;
+
+		cout<<e1<<endl;
+		cout<<e2<<endl;
+		cout<<e3<<endl;
 	}
 	
 	cout<<"EC"<<endl;
 	MTBF=12;
 	for(int i=0;i<3;i++)
         {
-       	double total = 10*365*24*60*60;
-        //int MTBF = 12;
-        	
-        if(i!=0)
-                MTBF = MTBF/2;
-        cout<<"MTBF = "<<MTBF<<endl;
-        double T = sqrt(2*(MTBF/(1-recomp))*60*60*chk);
-        int M = 10*365*24/MTBF;
-	double M1 = M*recomp;
-	double M2 = M*(1-recomp);
+		double total = 10*365*24*60*60;
+		//int MTBF = 12;
 
-        cout<<"T = "<<T<<endl;
-        double T_w = (3*(chk + T))/2*M2 + chk*0.5*M1;
-        int N1 = (total - T_w)/T +1;
-        int N2 = (total - T_w)/(T + 0.5*chk) +1;
-        int N3 = (total - T_w)/(T + chk) +1;
+		if(i!=0)
+			MTBF = MTBF/2;
+		cout<<"MTBF = "<<MTBF<<endl;
+		double T = sqrt(2*(MTBF/(1-recomp))*60*60*chk);
+		int M = 10*365*24/MTBF;
+		double M1 = M*recomp;
+		double M2 = M*(1-recomp);
 
-        double e1 = T*N1/total;
-        double e2 = T*N2/total;
-        double e3 = T*N3/total;
+		cout<<"T = "<<T<<endl;
+		double T_w = (3*(chk + T))/2*M2 + chk*0.5*M1;
+		int N1 = (total - T_w)/T +1;
+		int N2 = (total - T_w)/(T + 0.5*chk) +1;
+		int N3 = (total - T_w)/(T + chk) +1;
 
-        cout<<e1<<endl;
-        cout<<e2<<endl;
-        cout<<e3<<endl;
+		double e1 = T*N1/total;
+		double e2 = T*N2/total;
+		double e3 = T*N3/total;
+
+		cout<<e1<<endl;
+		cout<<e2<<endl;
+		cout<<e3<<endl;
 	}
 	return 0;
 }
